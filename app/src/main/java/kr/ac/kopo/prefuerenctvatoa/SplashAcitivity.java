c7427a63225a623794597ddd77a3f7ab3f07a8cd
wodyd456 <wodyd2131@gmail.com>
2026-06-11 14:55:45 +0900

    이미지 제목 이미지 화면 출력! 화면 전환!!!

---
+++ app/src/main/java/kr/ac/kopo/prefuerenctvatoa/SplashAcitivity.java
@@ -0,0 +1,29 @@
+package kr.ac.kopo.prefuerenctvatoa;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashAcitivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            Intent intent = new Intent(SplashAcitivity.this, MainActivity.class);
+            startActivity(intent);
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            finish();
+        }, 5000);
+
+    }
+}
